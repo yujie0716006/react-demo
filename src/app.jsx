c18b3d3420f25fa1fe975f4c5405f7c977f403ba
yujie <noneyujie_v@didichuxing.com>
2019-05-16 11:17:45 +0800

    react相关demo

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,25 @@
 import React, {Component} from 'react'
 
-import './app.css'
+import './app.less'
 
 export default class App extends Component {
+  state = {
+    active: false
+  }
   render () {
+    // const {active} = this.state
     return (
       <div>
         <div className="wrap">
-          <div className="inner"></div>
+          <div className="inner size">inner</div>
+          <div className="second active">second</div>
         </div>
+
+        <style jsx>{`
+          .active{
+            background-color: ${this.state.active ? 'black' : 'red'}
+          }
+          `}</style>
       </div>
     )
   }
